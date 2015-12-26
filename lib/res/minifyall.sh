5ea186059c9ff7dfa37cd9614daba64eaef49585
DavidAQ <self@davidaq.com>
2015-12-26 14:06:23 +0800

    uglify -> uglifyjs

--- lib/res/minifyall.sh
+++ lib/res/minifyall.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-find . -name \*.min.js -exec rm -f {} \; ; find . -name \*.js | while read x; do echo $x; uglify -s $x -o ${x:0:${#x}-3}.min.js >/dev/null; done
+find . -name \*.min.js -exec rm -f {} \; ; find . -name \*.js | while read x; do echo $x; uglifyjs $x -o ${x:0:${#x}-3}.min.js ; done
