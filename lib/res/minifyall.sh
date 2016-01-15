983f0403be14987645ac69b92aba320c0251af8f
DavidAQ <anqin@ccme.cc>
2016-01-15 15:53:09 +0800

    for iterator, hide $$$AWF$$$ key

--- lib/res/minifyall.sh
+++ lib/res/minifyall.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
+pushd `dirname $0` > /dev/null
 find . -name \*.min.js -exec rm -f {} \; ; find . -name \*.js | while read x; do echo $x; uglifyjs $x -c -m -o ${x:0:${#x}-3}.min.js ; done
 rm libPreset*.min.js
+popd > /dev/null
